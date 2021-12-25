654973c64ff5990cd1cc88298e983f6c0b7d77cd
Jeong_Jin_Hyo <74443267+jjhlove04@users.noreply.github.com>
2021-12-25 15:22:39 +0900

    add open

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,30 +7,42 @@ using DG.Tweening;
 public class UIManager : Singleton<UIManager>
 {
     [Header("책 펼치는 효과를 위한 변수")]
-    [SerializeField] private GameObject bookFront;
-    [SerializeField] private GameObject bookBack;
-    [SerializeField] private GameObject recipeBook; 
-    
-    private Sequence OpenRecipe = DOTween.Sequence(); //TODO:  시퀀스로 책 여는 연출 만들기
-    
+    public GameObject bookFront;
+    public Image bookBack;
+    public GameObject recipeBook;
+
+
+
+    Sequence mySequnce;//TODO:  시퀀스로 책 여는 연출 만들기
+
+
+    private void Start()
+    {
+
+        recipeBook.GetComponent<Button>().onClick.AddListener(() => OpenMenu());
+
+    }
+
     public void OpenMenu()
     {
         //버튼이 가운데로 움직이고 커진다음 회전으로 책 넘기는 효과.
-        OpenRecipe.Append(recipeBook.transform.DOMove(new Vector3(350,200,0),0.5f));
-        OpenRecipe.Append(recipeBook.transform.DOScale(new Vector3(8,8,0), 0.5f));
-        OpenRecipe.Append(recipeBook.transform.DOMove(new Vector3(450,200,0),1f).SetEase(Ease.OutQuart).SetDelay(0.7f));
+        mySequnce.Append(recipeBook.transform.DOMove(new Vector3(350, 200, 0), 0.5f));
+        mySequnce.Append(recipeBook.transform.DOScale(new Vector3(8, 8, 0), 0.5f));
+        mySequnce.Append(recipeBook.transform.DORotate(new Vector3(0, 90, 0), 0.8f).SetDelay(1f));
+        
+       
+        
 
-        bookBack.transform.position = recipeBook.transform.position;
+
     }
 
+    
     protected override void Awake() 
     {
         base.Awake();
-    }
 
-    private void Start() {
-        recipeBook.GetComponent<Button>().onClick.AddListener(() =>OpenMenu());
-
+        
+        
     }
 
     
